1dde9dacfa78dc03ea4710d06f096b8f3ea98b34
mdtaque <rifattaque@gmail.com>
2020-09-22 07:49:33 -0400

    mysql local connect

--- src/main/java/com/binarytech/employeeinfoservices/rest/EmployeeProfile.java
+++ src/main/java/com/binarytech/employeeinfoservices/rest/EmployeeProfile.java
@@ -4,11 +4,13 @@ import com.binarytech.employeeinfoservices.entity.Employee;
 import com.binarytech.employeeinfoservices.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class EmployeeProfile {
 
     public EmployeeService employeeService;
